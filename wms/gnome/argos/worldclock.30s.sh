7e3985c824d6ea5ef430bbf373ed070742117da8
Andreas Gerstmayr <agerstmayr@redhat.com>
2021-09-22 18:59:19 +0200

    update timezones

--- wms/gnome/argos/worldclock.30s.sh
+++ wms/gnome/argos/worldclock.30s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "$(TZ=America/Los_Angeles date +"SF %H:%M") / $(TZ=America/Chicago date +"HU %H:%M") / $(TZ=Australia/Melbourne date +"MEL %H:%M")"
+echo "$(TZ=America/Los_Angeles date +"SF %H:%M") / $(TZ=America/New_York date +"ET %H:%M") / $(TZ=Australia/Melbourne date +"MEL %H:%M")"
