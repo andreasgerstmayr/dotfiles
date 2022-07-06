9675af973f38cc66580e2b488849a6a389366b8c
Andreas Gerstmayr <agerstmayr@redhat.com>
2022-07-06 16:35:11 +0200

    worldclock: add UTC time

--- wms/gnome/argos/worldclock.30s.sh
+++ wms/gnome/argos/worldclock.30s.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "$(TZ=America/Los_Angeles date +"SF %H:%M") / $(TZ=America/New_York date +"GA %H:%M") / $(TZ=Australia/Melbourne date +"MEL %H:%M")"
+echo "$(TZ=UTC date +"UTC %H:%M") /" \
+     "$(TZ=America/Los_Angeles date +"SF %H:%M") / " \
+     "$(TZ=America/New_York date +"GA %H:%M") / " \
+     "$(TZ=Australia/Melbourne date +"MEL %H:%M")"
