380d55d1c53666ed4dc2255b000e48f351adcc40
Andreas Gerstmayr <agerstmayr@redhat.com>
2023-11-10 14:30:52 +0100

    add india time

--- wms/gnome/argos/worldclock.30s.sh
+++ wms/gnome/argos/worldclock.30s.sh
@@ -2,4 +2,5 @@
 
 echo "$(TZ=UTC date +"UTC %H:%M") /" \
      "$(TZ=America/Toronto date +"TO %H:%M") / " \
-     "$(TZ=America/Merida date +"ME %H:%M")"
+     "$(TZ=America/Merida date +"ME %H:%M") /" \
+     "$(TZ=Asia/Kolkata date +"IN %H:%M")"
