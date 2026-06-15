7334c5732db471abc6539102aa91cd318b9d27e9
Andreas Gerstmayr <agerstmayr@redhat.com>
2026-06-15 16:22:52 +0200

    add Chromium CDP app shortcut
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- apps/argos/worldclock.30s.sh
+++ apps/argos/worldclock.30s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$(TZ=UTC date +"UTC %H:%M") /" \
-     "$(TZ=America/Toronto date +"TO %H:%M") / " \
+     "$(TZ=America/New_York date +"BO %H:%M") / " \
      "$(TZ=America/Merida date +"ME %H:%M") /" \
      "$(TZ=Asia/Kolkata date +"IN %H:%M")"
